24d89653b8d3596eb47799fd82912749147cab1f
padeir0 <arturiurevf@gmail.com>
2026-01-13 22:08:02 -0300

    added pao_colors.h

--- test/common.h
+++ test/common.h
@@ -3,6 +3,7 @@
 #include "../src/pao_status.h"
 #include <strings.h>
 #include <string.h>
+#include "../src/pao_colors.h"
 
 #define DEFAULT_SIZE 2048
 
@@ -18,19 +19,13 @@ typedef struct {
   bool (*func)(void);
 } Tester;
 
-
-// TODO: create a header for colors
-#define COLOR_RED   "\x1b[31m"
-#define COLOR_BLUE  "\x1b[34m"
-#define COLOR_RESET "\x1b[0m"
-
 char print_buff[DEFAULT_SIZE];
 void test(Tester t) {
   bzero(print_buff, DEFAULT_SIZE);
   if (t.func()) {
-    strcat(print_buff, COLOR_BLUE "OK" COLOR_RESET ": ");
+    strcat(print_buff, PAO_colors_blue "OK" PAO_colors_reset ": ");
   } else {
-    strcat(print_buff, COLOR_RED "FAIL" COLOR_RESET ": ");
+    strcat(print_buff, PAO_colors_red "FAIL" PAO_colors_reset ": ");
   }
   strcat(print_buff, t.name);
   strcat(print_buff, "\n");
